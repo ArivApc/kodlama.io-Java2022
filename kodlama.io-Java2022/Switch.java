6a989b4f9c9a38066eaddc920c944e1a4810a975
ArivApc <104191114+ArivApc@users.noreply.github.com>
2022-09-28 15:16:35 +0300

    Add files via upload

---
+++ kodlama.io-Java2022/Switch.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	char grade = 'A';
+	
+	switch (grade){
+	    case  'A':
+	        System.out.println("mükemmel : geçtiniz");
+	        break;
+	   case 'B':
+	        System.out.println("çok güzel : geçtiniz");
+	        break;
+	   case 'C':
+	        System.out.println("iyi : geçtiniz");
+	        break;
+	   case 'D':
+	        System.out.println("fena değil : geçtiniz");
+	        break;
+	   case 'F':
+	        System.out.println("maalesef : kaldınız");
+	       break;
+	   default:
+	        System.out.println("geçersiz not girdiniz");
+	    
+	}
+		
+		
+		
+		
+	}
+}
